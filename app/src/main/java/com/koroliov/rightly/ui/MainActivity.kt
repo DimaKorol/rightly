ab04439122204aff37b79f59c75992f2faab23c8
Dima Korolev <karaliou.dzmitryi@gmail.com>
2017-07-04 01:29:14 +0300

    Added templates of main fragments.

--- app/src/main/java/com/koroliov/rightly/ui/MainActivity.kt
+++ app/src/main/java/com/koroliov/rightly/ui/MainActivity.kt
@@ -2,8 +2,14 @@ package com.koroliov.rightly.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 import com.koroliov.rightly.R
 import com.koroliov.rightly.R.id.*
+import com.koroliov.rightly.ui.feed.FeedFragment
+import com.koroliov.rightly.ui.location.LocationMainFragment
+import com.koroliov.rightly.ui.location.ProfileMainFragment
+import com.koroliov.rightly.ui.location.TimeMainFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,19 +17,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startFragment(FeedFragment())
         bottomNavigation.setOnNavigationItemSelectedListener {
             if (!it.isChecked) {
                 when (it.itemId) {
-                    homeSection -> true
-                    locationSection -> true
-                    timeSection -> true
-                    profileSection -> true
-                    else -> {
-                        false
-                    }
+                    homeSection -> startFragment(FeedFragment())
+                    locationSection -> startFragment(LocationMainFragment())
+                    timeSection -> startFragment(TimeMainFragment())
+                    profileSection -> startFragment(ProfileMainFragment())
+                    else -> false
                 }
             }
             true
         }
+    }
+
+    fun startFragment(fragment: Fragment) {
+        var fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        if (fragmentManager.backStackEntryCount > 0) {
+            fragmentManager.popBackStackImmediate()
+            fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+            fragmentTransaction.addToBackStack(null)
+        } else {
+            fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        }
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/koroliov/rightly/ui/time/TimeMainFragment.kt
@@ -0,0 +1,18 @@
+package com.koroliov.rightly.ui.location
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.koroliov.rightly.R
+
+/**
+ * Created by dima_korolev on 04/07/2017.
+ */
+
+class TimeMainFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_time_main, container, false)
+    }
+}
