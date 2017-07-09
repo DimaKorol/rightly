970fdeb2e98bba4433d68bb5aa5d4882c1d33a27
Dima Korolev <karaliou.dzmitryi@gmail.com>
2017-07-10 01:08:50 +0300

    Improved corner action button.

--- app/src/main/java/com/koroliov/rightly/ui/view/CornerActionButton.kt
+++ app/src/main/java/com/koroliov/rightly/ui/view/CornerActionButton.kt
@@ -5,6 +5,7 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.widget.ImageButton
 import by.liveboard.android.utils.toPx
+import com.koroliov.rightly.R
 
 
 /**
@@ -12,8 +13,23 @@ import by.liveboard.android.utils.toPx
  */
 
 class CornerActionButton : ImageButton {
+    enum class Corner(val i: Int) {
+        topLeft(0), topRight(1), bottomRight(2), bottomLeft(3);
+
+        companion object {
+            fun valueOf(i: Int): Corner = when(i) {
+                0 -> topLeft
+                1 -> topRight
+                2 -> bottomRight
+                3 -> bottomLeft
+                else -> topRight
+            }
+        }
+    }
+
     private var clipPath = Path()
     private var maskPaint = Paint()
+    private var corner: Corner = Corner.topRight
 
     constructor(context: Context) : this(context, null as AttributeSet)
 
@@ -21,15 +37,57 @@ class CornerActionButton : ImageButton {
         maskPaint.color = Color.WHITE
         maskPaint.isAntiAlias = true
         maskPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_IN)
+
+        val a = context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.CornerActionButton,
+                0, 0)
+
+        try {
+            corner = Corner.valueOf(a.getInt(R.styleable.CornerActionButton_corner, 1))
+        } finally {
+            a.recycle()
+        }
+
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        setPadding((w*0.25).toInt(), 0, 0, (h*0.25).toInt())
+
+        val verticalPadding = (h*0.25).toInt()
+        val horizontalPadding = (w*0.25).toInt()
+        setPadding(
+                when(corner) {
+                    Corner.bottomRight, Corner.topRight -> horizontalPadding
+                    else -> 0
+                },
+                when(corner) {
+                    Corner.bottomRight, Corner.bottomLeft -> verticalPadding
+                    else -> 0
+                },
+                when(corner) {
+                    Corner.topLeft, Corner.bottomLeft -> horizontalPadding
+                    else -> 0
+                },
+                when(corner) {
+                    Corner.topLeft, Corner.topRight -> verticalPadding
+                    else -> 0
+                })
+
+        val width = w.toFloat()
+        val height = h.toFloat()
+
+        val radii = when(corner) {
+            Corner.topLeft -> floatArrayOf(0f,0f,0f,0f,width, height,0f,0f)
+            Corner.topRight -> floatArrayOf(0f,0f,0f,0f,0f,0f,width, height)
+            Corner.bottomRight -> floatArrayOf(width,height,0f,0f,0f,0f,0f,0f)
+            Corner.bottomLeft -> floatArrayOf(0f,0f,width,height,0f,0f,0f,0f)
+        }
+
         clipPath.reset()
         clipPath.addRoundRect(
                 0f, 0f, w.toFloat(), h.toFloat(), // rectangle
-                floatArrayOf(0f,0f,0f,0f,0f,0f,w.toPx().toFloat(), h.toPx().toFloat()), // corner
+                radii, // corner
                 Path.Direction.CW)
         clipPath.close()
     }
